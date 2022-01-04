ce2a783be6c7597c1122aa78a7b4a54b26fc251f
imsocialcoder <39344644+imsocialcoder@users.noreply.github.com>
2022-01-04 15:23:26 +0300

    Create DivisibleSumPairs.java
    My solution to Divisible Sum Pairs challenge on hackerrank

---
+++ DivisibleSumPairs.java
@@ -0,0 +1,107 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+/*
+Given an array of integers and a positive integer , determine the number of  pairs where  and  +  is divisible by .
+
+Example
+
+
+
+Three pairs meet the criteria:  and .
+
+Function Description
+
+Complete the divisibleSumPairs function in the editor below.
+
+divisibleSumPairs has the following parameter(s):
+
+int n: the length of array 
+int ar[n]: an array of integers
+int k: the integer divisor
+Returns
+- int: the number of pairs
+
+Input Format
+
+The first line contains  space-separated integers,  and .
+The second line contains  space-separated integers, each a value of .
+
+Constraints
+
+Sample Input
+
+STDIN           Function
+-----           --------
+6 3             n = 6, k = 3
+1 3 2 6 1 2     ar = [1, 3, 2, 6, 1, 2]
+Sample Output
+
+ 5
+*/
+
+class Result {
+
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+    // Write your code here
+    int count = 0;
+    
+    for(int i=0; i<n-1; i++){
+        for(int j=i+1; j<n; j++){
+            if((ar.get(i)+ar.get(j))%k==0){
+                count++;
+            }else{
+                //do nothing.
+            }
+        }
+        
+    }
+    
+    return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = Result.divisibleSumPairs(n, k, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
