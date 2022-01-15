a73c4ef23f8bbbdcfbd0a22f37c00f68ecb7e888
imsocialcoder <39344644+imsocialcoder@users.noreply.github.com>
2022-01-15 22:34:17 +0300

    Mark and Toys challenge
    My solution to Mark and Toys challenge on hackerrank

---
+++ MarkAndToys.java
@@ -0,0 +1,112 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+/*
+
+Mark and Toys Challenge
+
+Mark and Jane are very happy after having their first child. Their son loves toys, so Mark wants to buy some. There are a number of different toys lying in front of him, tagged with their prices. Mark has only a certain amount to spend, and he wants to maximize the number of toys he buys with this money. Given a list of toy prices and an amount to spend, determine the maximum number of gifts he can buy.
+
+Note Each toy can be purchased only once.
+
+Example
+
+
+The budget is  units of currency. He can buy items that cost  for , or  for  units. The maximum is  items.
+
+Function Description
+
+Complete the function maximumToys in the editor below.
+
+maximumToys has the following parameter(s):
+
+int prices[n]: the toy prices
+int k: Mark's budget
+Returns
+
+int: the maximum number of toys
+Input Format
+
+The first line contains two integers,  and , the number of priced toys and the amount Mark has to spend.
+The next line contains  space-separated integers 
+
+Constraints
+
+
+
+
+A toy can't be bought multiple times.
+
+Sample Input
+
+7 50
+1 12 5 111 200 1000 10
+Sample Output
+
+4
+Explanation
+
+He can buy only  toys at most. These toys have the following prices: .
+*/
+class Result {
+
+    /*
+     * Complete the 'maximumToys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY prices
+     *  2. INTEGER k
+     */
+
+    public static int maximumToys(List<Integer> prices, int k) {
+        int toysToBuy = 0;
+        boolean isFinished = false;
+        Collections.sort(prices);
+        
+        for(int i=0; i<prices.size() &&!isFinished; i++){
+            if(k>=prices.get(i)){
+                toysToBuy++;
+                k = k - prices.get(i);
+            }else{
+                isFinished = true;
+            }
+        }
+        
+        return toysToBuy;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> prices = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.maximumToys(prices, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
